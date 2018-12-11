e38f3ffdfb303e62698a5123028cbc2e3e0880f7
Nero <Nero@NERO>
2018-12-11 23:32:15 +0100

    Implementation complete

--- Calculator/ClientApp/src/app/app.component.ts
+++ Calculator/ClientApp/src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'calculator';
+  expression: string = "";
+  Http: HttpClient;
+  baseUrl: string;
+
+  constructor(http: HttpClient, @Inject('BASE_URL') BaseUrl: string) {
+    this.Http = http;
+    this.baseUrl = BaseUrl;
+  }
+
+  public Result = "";
+
+  Click = (value: string) => {
+    this.Result += value;
+    this.expression += value;
+  }
+
+  Cclick = () => {
+    this.Result = "";
+    this.expression = "";
+  }
+
+  EqualClick() {
+
+    this.Http.post<string>(this.baseUrl + 'calculator/Calculate', { "expression": this.expression }, { responseType: "json" }).subscribe(result => {
+      this.Result = result;
+    }, error => console.error(error));
+  }
 }
--- Calculator/Controllers/SampleDataController.cs
+++ Calculator/Controllers/SampleDataController.cs
@@ -1,44 +1,31 @@
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Calculator.Controllers
 {
-    [Route("api/[controller]")]
-    public class SampleDataController : Controller
+    [Route("/calculator")]
+    public class CalculatorController : Controller
     {
-        private static string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
 
-        [HttpGet("[action]")]
-        public IEnumerable<WeatherForecast> WeatherForecasts()
+        [HttpPost("[action]")]
+        public string Calculate([FromBody] Expression expression)
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                DateFormatted = DateTime.Now.AddDays(index).ToString("d"),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            });
+            return Eval(expression.expression).ToString();
         }
 
-        public class WeatherForecast
+        static Double Eval(String expression)
         {
-            public string DateFormatted { get; set; }
-            public int TemperatureC { get; set; }
-            public string Summary { get; set; }
-
-            public int TemperatureF
-            {
-                get
-                {
-                    return 32 + (int)(TemperatureC / 0.5556);
-                }
-            }
+            DataTable tempsource = new DataTable();
+            return Convert.ToDouble(tempsource.Compute(expression, string.Empty));
         }
+    }
+
+    public class Expression
+    {
+        public string expression { get; set; }
     }
 }
